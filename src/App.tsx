b8874f367fa1399934ec356d72920021c1432bc2
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-15 21:34:56 +0100

    Implement pattern 6

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
 
 import "./App.css";
-import vertexShader from "./shaders/pattern_5/vertex.glsl";
-import fragmentShader from "./shaders/pattern_5/fragment.glsl";
+import vertexShader from "./shaders/pattern_06/vertex.glsl";
+import fragmentShader from "./shaders/pattern_06/fragment.glsl";
 
 const current = {
   vertexShader,
