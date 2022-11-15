ef91d77547be13ba48dc7706c21471aebf65ae68
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-15 21:52:06 +0100

    Implement pattern 11

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
 
 import "./App.css";
-import vertexShader from "./shaders/pattern_10/vertex.glsl";
-import fragmentShader from "./shaders/pattern_10/fragment.glsl";
+import vertexShader from "./shaders/pattern_11/vertex.glsl";
+import fragmentShader from "./shaders/pattern_11/fragment.glsl";
 
 const current = {
   vertexShader,
---
+++ src/shaders/pattern_11/fragment.glsl
@@ -0,0 +1,15 @@
+varying vec2 vUv;
+
+void main()
+{
+    // float strength = max(
+    //     mod(vUv.x * 10.0, 1.0),
+    //     mod(vUv.y * 10.0, 1.0)
+    // );
+    // strength = step(0.8, strength);
+
+    float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+    strength += step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
+}
