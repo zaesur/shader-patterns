42186c5e35e437ab4596c929e729c24496d842b1
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-19 18:34:12 +0100

    Implement pattern 25

---
+++ src/shaders/pattern_25/fragment.glsl
@@ -0,0 +1,17 @@
+varying vec2 vUv;
+
+float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+void main()
+{
+    vec2 gridUv = vec2(
+        floor(vUv.x * 10.0) / 10.0,
+        floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0
+    );
+    float strength = random(gridUv);
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
+}
