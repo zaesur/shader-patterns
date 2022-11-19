25583eaf88864b44858f3ff0e0f3de878ac97e65
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-19 18:25:57 +0100

    Implement pattern 23

---
+++ src/shaders/pattern_23/fragment.glsl
@@ -0,0 +1,13 @@
+varying vec2 vUv;
+
+float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+void main()
+{
+    float strength = random(vUv);
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
+}
