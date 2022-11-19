18b868bd6f18f716b5e59320b1a70d698edb6214
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-19 17:48:38 +0100

    Implement pattern 20

---
+++ src/shaders/pattern_20/fragment.glsl
@@ -0,0 +1,15 @@
+varying vec2 vUv;
+
+void main()
+{
+    float strength = step(
+        0.2,
+        max(abs(vUv.x - 0.5), abs(vUv.y - 0.5))
+    );
+    strength *= 1.0 - step(
+        0.25,
+        max(abs(vUv.x - 0.5), abs(vUv.y - 0.5))
+    );
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
+}
